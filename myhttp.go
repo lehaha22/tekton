8c1a68db9d78c288c1ba4253f4e9e3864fb75fe6
赵乐(10035389) <zhaol-x@glodon.com>
2021-09-03 14:03:16 +0800

    增加请求日志记录

--- myhttp.go
+++ myhttp.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-        "fmt"
-        "net/http"
+	"fmt"
+	"log"
+	"net/http"
 	"sort"
 )
 
-func Countstr(str string)  (result map[int]int) {
+func Countstr(str string) (result map[int]int) {
 
 	map1 := make(map[int]int)
 	for i := 0; i < len(str); i++ {
@@ -27,25 +28,36 @@ func Countstr(str string)  (result map[int]int) {
 	}
 	sort.Ints(keys)
 	result = make(map[int]int)
-	for _ ,key := range keys {
-		fmt.Printf("%c : %d \n",key,map1[key])
+	for _, key := range keys {
+		fmt.Printf("%c : %d \n", key, map1[key])
 		result[key] = map1[key]
-
 
 	}
 	return
 }
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintln(w, "Hello golang!")
+	fmt.Fprintln(w, "Hello golang!")
+	log.Printf("远程地址:%v,请求host:%v,请求方法:%v", r.RemoteAddr, r.Host, r.Method)
 }
 
+//type  BaseHander struct {
+//
+//}
+//
+//func (hander *BaseHander)ServeHTTP(resp http.ResponseWriter,req *http.Request){
+//	fmt.Println("url path=>",req.Host)
+//	fmt.Println("url param a =>",req.URL.Query().Get("a"))
+//	resp.Write([]byte("hello worlds"))
+//}
+
 func main() {
-        http.HandleFunc("/", sayHello)
-        fmt.Println("start 127.0.0.1:8080...")
-        err := http.ListenAndServe(":8080", nil)
-        if err != nil {
-                fmt.Printf("http server failed, err:%v\n", err)
-                return
-        }
+
+	log.Println("start 127.0.0.1:8080...")
+	http.HandleFunc("/hello", sayHello)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Printf("http server failed, err:%v\n", err)
+		return
+	}
 }
